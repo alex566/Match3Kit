6340e80ff4d06d7e2b06b7ec6be2185b25ee7186
Alexey Oleynik <alexey566@icloud.com>
2020-05-01 18:42:29 +0300

    Added product

--- Package.swift
+++ Package.swift
@@ -3,6 +3,9 @@ import PackageDescription
 
 let package = Package(
     name: "Match3Kit",
+    products: [
+        .library(name: "Match3Kit", targets: ["Match3Kit"])
+    ],
     targets: [
         .target(name: "Match3Kit"),
         .testTarget(name: "Match3KitTests"),
